ca4023e1a3f0b392b20285e2a8bdaee6c9159153
arguad <rickyguadagnin@gmail.com>
2022-02-01 22:34:28 -0800

    Update start_miners.sh
    Increment port numbers by 100

--- start_miners.sh
+++ start_miners.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in 1 2 3 4; do
- pm2 start ~/.bittensor/bittensor/bittensor/_neuron/text/advanced_server/main.py --name miner00$i --time --interpreter python3 -- --logging.debug --subtensor.network nakamoto --neuron.restart true --server.model_name distilgpt2 --axon.port 8091 --wallet.name bittensor-wallet-`hostname` --wallet.hotkey hotkey$i-`hostname`
+ pm2 start ~/.bittensor/bittensor/bittensor/_neuron/text/advanced_server/main.py --name miner00$i --time --interpreter python3 -- --logging.debug --subtensor.network nakamoto --neuron.restart true --server.model_name distilgpt2 --axon.port 8${i}91 --wallet.name bittensor-wallet-`hostname` --wallet.hotkey hotkey$i-`hostname`
 done
 
 
